ca1956b6117cdd082c205522bb4c1d98995e8e83
Sekanin <131435559+Sekanin@users.noreply.github.com>
2024-08-07 13:30:04 +0300

    Upd

---
+++ Assets/_Project/Scripts/ActiveTeleportationRay.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
+
+public class ActivateTeleportationRay : MonoBehaviour
+{
+    public GameObject leftTeleportation;
+    public GameObject rightTeleportation;
+
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumer, out bool leftValid);
+        
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rihgtPos, out Vector3 rihgtNormal, out int rihgtNumer, out bool rihgtValid);
+        
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+    }
+}
