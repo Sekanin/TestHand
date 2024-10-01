c401d812c4534e3ed022a59ec403456f19baf4b0
Sekanin <131435559+Sekanin@users.noreply.github.com>
2024-10-01 10:42:26 +0300

    Test

---
+++ Assets/Devissage.cs
@@ -0,0 +1,99 @@
+/*using UnityEngine;
+using UnityEngine.UI;
+
+public class BoltUnscrewer : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject[] bolts;
+
+    void Start()
+    {
+        yourButton.onClick.AddListener(UnscrewBolts);
+    }
+
+    void UnscrewBolts()
+    {
+        foreach (GameObject bolt in bolts)
+        {
+            bolt.transform.Rotate(0, 0, 90);
+        }
+    }
+}
+*/
+
+/*using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BoltUnscrewer : MonoBehaviour
+{
+    public Button yourButton; 
+    public GameObject[] bolts;
+    public float unscrewSpeed = 90f;
+    public float moveSpeed = 0.1f;
+
+    void Start()
+    {
+        yourButton.onClick.AddListener(UnscrewBolts);
+    }
+
+    void UnscrewBolts()
+    {
+        foreach (GameObject bolt in bolts)
+        {
+            StartCoroutine(UnscrewBolt(bolt));
+        }
+    }
+
+    IEnumerator UnscrewBolt(GameObject bolt)
+    {
+        while (true)
+        {
+            bolt.transform.Rotate(0, 0,  unscrewSpeed * Time.deltaTime);
+            
+            bolt.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            
+            yield return null;
+        }
+    }
+}*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BoltUnscrewer : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject[] bolts;
+    public float unscrewSpeed = 90f;
+    public float moveSpeed = 0.1f;
+    public float maxDistance = 0.1f;
+
+    void Start()
+    {
+        yourButton.onClick.AddListener(UnscrewBolts);
+    }
+
+    void UnscrewBolts()
+    {
+        foreach (GameObject bolt in bolts)
+        {
+            StartCoroutine(UnscrewAndMoveBolt(bolt));
+        }
+    }
+
+    IEnumerator UnscrewAndMoveBolt(GameObject bolt)
+    {
+        Vector3 startPosition = bolt.transform.position;
+        while (Vector3.Distance(startPosition, bolt.transform.position) < maxDistance)
+        {
+            bolt.transform.Rotate(0, 0, unscrewSpeed * Time.deltaTime);
+            
+            bolt.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            
+            yield return null;
+        }
+    }
+}
+
+
