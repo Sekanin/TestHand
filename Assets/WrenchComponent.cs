c6a7788446f8251ed975165eee188c606aebf2ee
Sekanin <131435559+Sekanin@users.noreply.github.com>
2024-10-16 10:57:15 +0300

    test

--- Assets/WrenchComponent.cs
+++ Assets/WrenchComponent.cs
@@ -1,4 +1,53 @@
 using UnityEngine;
+using UnityEngine.XR;
+
+public class WrenchVR : MonoBehaviour
+{
+    private Bolt currentBolt;
+    private Vector3 lastControllerPosition;
+    private Quaternion lastControllerRotation;
+
+    void Update()
+    {
+        if (currentBolt != null)
+        {
+            Vector3 currentControllerPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+            Quaternion currentControllerRotation = InputTracking.GetLocalRotation(XRNode.RightHand);
+
+            Vector3 deltaPosition = currentControllerPosition - lastControllerPosition;
+            float deltaRotation = Quaternion.Angle(lastControllerRotation, currentControllerRotation);
+
+            if (deltaRotation > 0.1f) // Ajoute une tolérance pour éviter les mouvements accidentels
+            {
+                float rotationDirection = Mathf.Sign(Vector3.Dot(deltaPosition, transform.right));
+                currentBolt.Unscrew(rotationDirection);
+            }
+
+            lastControllerPosition = currentControllerPosition;
+            lastControllerRotation = currentControllerRotation;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bolt"))
+        {
+            currentBolt = other.gameObject.GetComponent<Bolt>();
+            lastControllerPosition = InputTracking.GetLocalPosition(XRNode.RightHand);
+            lastControllerRotation = InputTracking.GetLocalRotation(XRNode.RightHand);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bolt"))
+        {
+            currentBolt = null;
+        }
+    }
+}
+/*
+ * using UnityEngine;
 
 public class Wrench : MonoBehaviour
 {
@@ -36,5 +85,7 @@ public class Wrench : MonoBehaviour
         }
     }
 }
+
+ */
 
 
